957210c6d07984370f4944ba5dee75bd9573f8e9
Jack Meng <jackmeng0814@gmail.com>
2023-08-21 01:48:54 -0400

    `test`

--- js/workerController.js
+++ js/workerController.js
@@ -2,6 +2,14 @@ function workerController_openURL(url) {
   window.open(url, "_blank");
 }
 
+function workerController_openURLCurrent(url) {
+  window.open(url);
+}
+
 function workerController_exoadProfilePage() {
   workerController_openURL("https://github.com/exoad");
+}
+
+function workerController_legalsPage() {
+  workerController_openURLCurrent("../page/legals");
 }
